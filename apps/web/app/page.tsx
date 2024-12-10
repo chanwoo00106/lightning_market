f953b2a52267a842f385a51a8dedc80dfce4bfac
chanwoo00106 <byun00106@gmail.com>
2024-12-10 16:56:45 +0900

    feat: 🎸 add shadcn components
    I added shadcn config

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,14 @@
+import { Button } from '@repo/ui/components/ui/button';
+import { Input } from '@repo/ui/components/ui/input';
+
 export default function Home() {
-  return <div className="text-4xl">hello world</div>;
+  return (
+    <div className='space-y-4'>
+      <p className="text-4xl">Hello world</p>
+
+      <Button className='block'>button test</Button>
+
+      <Input defaultValue="Input test" />
+    </div>
+  );
 }
